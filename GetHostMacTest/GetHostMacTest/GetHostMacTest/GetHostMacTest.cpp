6e0f9a565191590ed46f5f30a3256e1a13424103
liguanlin <liguanlin@orbbec.com>
2019-03-13 15:09:28 +0800

    add get localhost mac demo

---
+++ GetHostMacTest/GetHostMacTest/GetHostMacTest/GetHostMacTest.cpp
@@ -0,0 +1,75 @@
+// GetHostMacTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <winsock2.h>
+#include <iphlpapi.h>
+#include <string>
+#include <iostream>
+
+#pragma comment(lib, "IPHLPAPI.lib")
+
+bool GetMacByGetAdaptersInfo(std::string& macOUT)
+{
+	bool ret = false;
+
+	ULONG ulOutBufLen = sizeof(IP_ADAPTER_INFO);
+	PIP_ADAPTER_INFO pAdapterInfo = (IP_ADAPTER_INFO*)malloc(sizeof(IP_ADAPTER_INFO));
+	if (pAdapterInfo == NULL)
+		return false;
+	// Make an initial call to GetAdaptersInfo to get the necessary size into the ulOutBufLen variable
+	if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == ERROR_BUFFER_OVERFLOW)
+	{
+		free(pAdapterInfo);
+		pAdapterInfo = (IP_ADAPTER_INFO *)malloc(ulOutBufLen);
+		if (pAdapterInfo == NULL)
+			return false;
+	}
+
+	if (GetAdaptersInfo(pAdapterInfo, &ulOutBufLen) == NO_ERROR)
+	{
+		for (PIP_ADAPTER_INFO pAdapter = pAdapterInfo; pAdapter != NULL; pAdapter = pAdapter->Next)
+		{
+			// 确保是以太网
+			if (pAdapter->Type != MIB_IF_TYPE_ETHERNET)
+				continue;
+			// 确保MAC地址的长度为 00-00-00-00-00-00
+			if (pAdapter->AddressLength != 6)
+				continue;
+			char acMAC[32];
+			sprintf_s(acMAC, "%02X-%02X-%02X-%02X-%02X-%02X",
+				int(pAdapter->Address[0]),
+				int(pAdapter->Address[1]),
+				int(pAdapter->Address[2]),
+				int(pAdapter->Address[3]),
+				int(pAdapter->Address[4]),
+				int(pAdapter->Address[5]));
+			macOUT = acMAC;
+			ret = true;
+			break;
+		}
+	}
+
+	free(pAdapterInfo);
+	return ret;
+}
+
+int main()
+{
+	std::string localhostMac;
+
+	int ulRet = GetMacByGetAdaptersInfo(localhostMac);
+	if (false == ulRet)
+	{
+		std::cout << "get mac error" << std::endl;
+
+		return 1;
+	}
+
+	std::cout << localhostMac;
+
+	system("pause");
+
+    return 0;
+}
+
