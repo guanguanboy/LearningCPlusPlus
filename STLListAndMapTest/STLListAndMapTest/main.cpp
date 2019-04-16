5f11fa6add8a191a650e9de96ebc73d4d31f0e0c
liguanlin <liguanlin@orbbec.com>
2019-04-16 11:45:50 +0800

    and stl vector test

--- STLListAndMapTest/STLListAndMapTest/main.cpp
+++ STLListAndMapTest/STLListAndMapTest/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <list>
+#include <vector>
 
 using namespace std;
 
@@ -41,7 +42,7 @@ int main()
 #endif
 
 //map 嵌套使用
-#if 1
+#if 0
 int main()
 {
 	map<int, map<int, int> > nestedMap;
@@ -134,4 +135,100 @@ int main()
 
 	system("pause");
 }
+#endif
+
+#if 0
+
+void printList(list<int>& templist)
+{
+	//it = templist.begin();
+
+	for (list<int>::iterator it = templist.begin(); it != templist.end(); it++)
+	{
+		cout << ' '<< *it;
+	}
+
+	cout << endl;
+}
+
+int main()
+{
+	list<int> mylist;
+	list<int>::iterator it;
+
+	for (int i = 1; i <= 5; i++)
+	{
+		mylist.push_back(i); // 1 2 3 4 5
+	}
+
+	printList(mylist);
+
+	it = mylist.begin();
+	++it; //it points now to number 2
+
+	mylist.insert(it, 10); // 1 10 2 3 4 5
+
+	printList(mylist);
+
+	mylist.insert(it, 2, 20); // "it" still points to number 2, 1 10 20 20 2 3 4 5
+
+	printList(mylist);
+
+	--it; // it points noew to the second 20
+
+	vector<int> myvector(2, 30);
+	mylist.insert(it, myvector.begin(), myvector.end()); // 1 10 20 30 30 20 2 3 4 5
+
+	cout << "mylist contains:";
+
+	printList(mylist);
+	
+	system("pause");
+
+	return 0;
+}
+
+#endif
+
+#if 1
+
+//vector<int> *kkk;//vector向量指针**
+
+int main()
+{
+	vector<int> *k;//vector向量指针  
+	k = new vector<int>[5];
+	//相当于int *p = new int[5];即vector<int> *kkk=new vector<int>[5];  
+	for (int i = 0; i<5; i++)
+	{
+		for (int j = 0; j<10; j++)
+		{
+			k[i].push_back(j);//像向量指针中追加值  
+		}
+	}
+	for (int i = 0; i<5; i++)
+	{
+		for (int j = 0; j < k[i].size(); j++)
+			cout << k[i][j] << "  ";
+		cout << endl;
+	}
+	delete[] k;
+
+	vector<int> *pVec;
+	pVec = new vector<int>;
+
+	for (int i = 0; i < 20; i++)
+	{
+		pVec->push_back(i);
+	}
+
+	for (vector<int>::iterator it = pVec->begin(); it != pVec->end(); it++)
+	{
+		cout << *it << " ";
+	}
+
+	delete pVec;
+	system("pause");
+}
+
 #endif
